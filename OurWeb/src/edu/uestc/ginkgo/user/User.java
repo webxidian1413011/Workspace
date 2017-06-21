2cf50d42af056a809563b4c9e0ee30122af28ca0
zxh004 <1652094443zxh@sina.com>
2017-06-21 21:24:25 +0800

    代码

---
+++ OurWeb/src/edu/uestc/ginkgo/user/User.java
@@ -0,0 +1,39 @@
+package edu.uestc.ginkgo.user;
+
+public class User {
+	    private int id;
+		private String username;
+		private String password;
+		public User(){
+			
+		}
+		
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public User(String username, String password) {
+			this.username=username;
+			this.password=password;
+		}
+		public String getUsername() {
+			return username;
+		}
+		public void setUsername(String username) {
+			this.username = username;
+		}
+
+		public String getPassword() {
+			return password;
+		}
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
+		
+		
+}
